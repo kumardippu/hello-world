76d40e0a7911da091fec0d25dff4a0cb6500252f
Dippu Kumar <kumardippu@gmail.com>
2023-01-20 10:45:27 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,10 @@
-# This is just for testing purpose
+"""
+This module contains a function that prints "Hello World" to the console.
+"""
 def print_hello():
+    """
+    Prints "Hello World" to the console.
+    """
     print("Hello World")
 
 print_hello()
